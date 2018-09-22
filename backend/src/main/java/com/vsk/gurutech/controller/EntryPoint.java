eed9972a14b180f96853cae5e8dbe26dbab67477
aramaniuk <aramaniuk@netgear.com>
2018-09-22 13:05:40 +0300

    pushed to subfolder

---
+++ backend/src/main/java/com/vsk/gurutech/controller/EntryPoint.java
@@ -0,0 +1,18 @@
+package com.vsk.gurutech.controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/entry-point")
+public class EntryPoint {
+
+    @GET
+    @Path("test")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String test() {
+        return "Test";
+    }
+
+}
